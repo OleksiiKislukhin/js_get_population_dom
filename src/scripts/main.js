273a5eaa83bd593e7dd055307b84427e767191da
Oleksii Kislukhin <leshka_k@ukr.net>
2022-03-20 22:22:11 +0200

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+function numberToString(number) {
+  let newNumber = number;
+  let resultStr = '';
+
+  while (newNumber > 0) {
+    resultStr = newNumber % 1000 + ',' + resultStr;
+    newNumber = Math.floor(newNumber / 1000);
+  }
+
+  return resultStr.replace(/,$/, '');
+}
+
+const populationList = document.querySelectorAll('[class="population"]');
+
+let sumPopulation = 0;
+
+for (const elem of populationList) {
+  sumPopulation += Number(elem.innerText.replace(/,/g, ''));
+}
+
+const totalElem = document.querySelector('[class="total-population"]');
+
+totalElem.innerText = numberToString(sumPopulation);
+
+const averageElem = document.querySelector('[class="average-population"]');
+
+averageElem.innerText = numberToString(
+  Math.floor(sumPopulation / populationList.length)
+);
